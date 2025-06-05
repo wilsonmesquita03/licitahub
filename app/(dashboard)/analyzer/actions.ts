14e606d09070093810853c81421abb6ad719ef55
wilsonmesquita03 <wilsonbrasilplauforever@gmail.com>
2025-06-05 10:06:15 -0300

    fix: translate title prompt to ptBR

--- app/(dashboard)/analyzer/actions.ts
+++ app/(dashboard)/analyzer/actions.ts
@@ -20,10 +20,10 @@ export async function generateTitleFromUserMessage({
   const { text: title } = await generateText({
     model: myProvider.languageModel("title-model"),
     system: `\n
-    - you will generate a short title based on the first message a user begins a conversation with
-    - ensure it is not more than 80 characters long
-    - the title should be a summary of the user's message
-    - do not use quotes or colons`,
+      - você irá gerar um título curto com base na primeira mensagem que um usuário usa para iniciar uma conversa  
+      - certifique-se de que o título tenha no máximo 80 caracteres  
+      - o título deve ser um resumo da mensagem do usuário  
+      - não use aspas nem dois-pontos`,
     prompt: JSON.stringify(message),
   });
 
