a2b8f36622542a1e61d1d714bf1fc15182d5b1b5
wilsonmesquita03 <wilsonbrasilplauforever@gmail.com>
2025-06-09 16:57:37 -0300

    feat: prisma acelerate

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,8 +1,14 @@
 // lib/prisma.ts
 import { PrismaClient } from "@/prisma/generated/prisma";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient;
+};
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+const prisma =
+  globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export default prisma;
